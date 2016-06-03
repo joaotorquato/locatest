70fc8ff74679ca0a9ee85ed306df2fa994291be4
João Torquato <joao.otl@gmail.com>
2016-06-02 22:12:09 -0300

    Add scenario User filters tweets ordered based on followers, retweets and favorites * add scenario on feature spec for tweets order * add model test on tweets_spec for tweets order

--- spec/features/user/user_filters_locaweb_mentions_spec.rb
+++ spec/features/user/user_filters_locaweb_mentions_spec.rb
@@ -12,7 +12,7 @@ feature 'User filters locaweb\'s mentions on twitter' do
     click_on 'Filter Locaweb\'s mentions on twitter'
 
     tweets_locaweb.each do |tweet|
-      expect(page).to have_link tweet.screen_name
+      expect(page).to have_link "@#{tweet.screen_name}"
       expect(page).to have_link "#{time_ago_in_words(tweet.created_at)} ago"
       expect(page).to have_content tweet.text
       expect(page).to have_content "Number of Retweets: #{tweet.retweet_count}"
@@ -23,8 +23,37 @@ feature 'User filters locaweb\'s mentions on twitter' do
     end
 
     tweets_without_locaweb.each do |tweet|
-      expect(page).not_to have_link tweet.screen_name
+      expect(page).not_to have_link "@#{tweet.screen_name}"
       expect(page).not_to have_content tweet.text
+    end
+  end
+
+  scenario 'ordered based on followers, retweets and favorites' do
+    allow(Tweet).to receive(:request)
+      .and_return(mock_tweets_without_locaweb + mock_tweets_locaweb)
+
+    visit root_path
+
+    click_on 'Filter Locaweb\'s mentions on twitter'
+
+    within('.tweet_0') do
+      expect(page).to have_link '@mrs_brooks_hahn'
+      expect(page)
+        .to have_link "#{time_ago_in_words('Mon Sep 24 03:35:21 2012')} ago"
+      expect(page).to have_content '@locaweb this is a tweet test 2'
+      expect(page).to have_content 'Number of Retweets: 10'
+      expect(page).to have_content 'Number of Followers: 2000'
+      expect(page).to have_content 'Number of Favorites: 7'
+    end
+
+    within('.tweet_1') do
+      expect(page).to have_link '@gerlach_madisen'
+      expect(page)
+        .to have_link "#{time_ago_in_words('Mon Sep 24 03:35:21 2012')} ago"
+      expect(page).to have_content '@locaweb this is a tweet test 1'
+      expect(page).to have_content 'Number of Retweets: 4'
+      expect(page).to have_content 'Number of Followers: 1000'
+      expect(page).to have_content 'Number of Favorites: 2'
     end
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,6 +8,19 @@ describe Tweet, '.filter_by' do
 
     tweets = Tweet.filter_by
 
-    expect(tweets).to eq tweets_locaweb
+    tweets.each do |tweet|
+      expect(tweets_locaweb.include?(tweet)).to be true
+    end
+  end
+
+  it 'should order based on followers, retweets and favorites' do
+    tweets_locaweb = mock_tweets_locaweb
+    allow(Tweet).to receive(:request)
+      .and_return(mock_tweets_without_locaweb + tweets_locaweb)
+
+    tweets = Tweet.filter_by
+
+    expect(tweets.first.screen_name).to eq 'mrs_brooks_hahn'
+    expect(tweets.second.screen_name).to eq 'gerlach_madisen'
   end
 end
